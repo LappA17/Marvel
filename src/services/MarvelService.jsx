cf96333c0f941a2bc297d6f2772f35223cfbdda1
Ruslan <ruslan.postoyuk@gmail.com>
2022-02-27 23:56:57 +0100

    113. Финальная оптимизация

--- src/services/MarvelService.jsx
+++ src/services/MarvelService.jsx
@@ -12,31 +12,27 @@ class MarvelService {
         return await res.json();
     }
 
-    getAllCharacters = () => {
-        return this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    getAllCharacters = async () => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter)
     }
+    /* res в этом случае это БОЛЬШОЙ ОБЪЕКТ ГДЕ ХРАНИТСЯ МАССИВ С РЕЗУЛЬТАТАМИ
+       Так как это массив, то мы можем применить метод map() - ЧТО БЫ СФОРМИРОВАТЬ МАССИВ С НОВЫМИ ОБЪЕКТАМИ */
 
     getCharacter = async (id) => {
         const res =  await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
-        return this._transformCharacter(res)
+        return this._transformCharacter(res.data.results[0])
     }
-    /* Теперь мы будем помещать результат в переменную и нужно заметить что фция getResource - это асинхронная ф-ция
-        И ЭТОТ res ЭТО ТОТ БОЛЬШОЙ ОБЪЕКТ КОТОРЫЙ МЫ ПОЛУЧАЕМ ! 
-        ТЕПЕРЬ ИЗ ЭТОГО БОЛЬШОГО ОБЪЕКТА БЛАГОДАРЯ НАШЕЙ ФЦИИ _transformCharacter НАМ ВОЗВРАЩАЮТСЯ КРАСИВЫЕ И ЧИСТЫЕ ДАННЫЕ КОТОРЫЕ
-НАМ ТОЛЬКО И НУЖНЫ */
-
-    /* Здесь мы будем получать данные и трансформировать его в объект и это основная тема данного урока, когда мы с api получаем кучу
-данных из которых нам многие не нужны просто, трансформировать их в те что нам нужны.
-    Мы будем возвращать объект, потому что мы получили на вход объект и так же будем его возвращать. ТОЛЬКО ЕСЛИ НА ВХОДЕ МЫ ПОЛУЧАЕМ
-ОГРОМНЫЙ ОБЪЕКТ С КУЧЕЙ НЕ НУЖНОЙ НАМ ИНФЫ, ТО ВОЗВРАЩАТЬ МЫ БУДЕМ ТОЛЬКО ТОТ КОТОРЫ НАС ИНТЕРЕСУЕТ. ТОЕСТЬ КАК res НАМ ПРИХОДИТ БОЛЬШОЙ
-ОБЪЕКТ А ВЫХОДИТ ТОТ КОТОРЫЙ НАМ ДЕЙСТВИТЕЛЬНО НУЖЕН ! - ЭТО И ЕСТЬ ТРАНСФОРМАЦИЯ ДАННЫХ */
-    _transformCharacter = (res) => {
+    /* А сюда помещаем повторяющейся участок кода */
+    
+    /* Теперь я говорю что здесь будет приходить объект персонажа */
+    _transformCharacter = (char) => {
         return {
-            name: res.data.results[0].name,
-            description: res.data.results[0].description,
-            thumbnail: res.data.results[0].thumbnail.path + '.' + res.data.results[0].thumbnail.extension,
-            homepage: res.data.results[0].urls[0].url,
-            wiki: res.data.results[0].urls[1].url
+            name: char.name,
+            description: char.description,
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url
         }
     }
 }
