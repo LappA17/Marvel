431249525ff12ebd0137c1288cc76193b7eb3063
Ruslan <ruslan.postoyuk@gmail.com>
2022-02-28 21:40:43 +0100

    114. Хороший тон приложения (спиннер, ошибки...)

--- src/components/randomChar/RandomChar.jsx
+++ src/components/randomChar/RandomChar.jsx
@@ -2,6 +2,8 @@ import { Component } from 'react';
 import MarvelService from '../../services/MarvelService'; 
 import './randomChar.scss';
 import mjolnir from '../../resources/img/mjolnir.png';
+import Spinner from '../spinner/Spinner';
+import ErrorMessage from '../errorMessage/ErrorMessage';
 
 class RandomChar extends Component {
     constructor(props) {
@@ -9,63 +11,70 @@ class RandomChar extends Component {
         this.updateChar()
     }
 
-/*  Можно так же доработать наш стейт, сейчас здесь лежат свойства именни, описания и тд, но в будущем здесь могут лежать и другие 
-данные, например какая-то ошибка или состояние загрузки */
+    /* Как Ваня и говорил у нас появились новые свойства которые мы должны теперь использовать */
     state = {
-        char: {}
-    }
-
+        char: {},
+        loading: true,
+        error: false // Исправим ошибку когда персонаж 404 и ломается страница
+    } /* После того как мы закончили в ErrorMessage у нас сейчас три состояние: загрузка персонажа, загрузка, ошибка */
     
     marvelService = new MarvelService();
 
-    /* Так мы оптимизируем код и res => {this.setState(res)}) уже не понадобиться */
-    onChatLoaded = (char) => {
-        this.setState({char}) // помещаем char потому что это скорощение от char: char
+    /* Передаем loading в false, что бы как только у нас загружаются данные, то у нас, то лоудинг будет в фолс автоматом.
+Это легко проговорить словами, если у нас идет загрузка то там где state выше у нас лоудинг стоит в тру, как только данные
+у нас загрузились то лоудинг в фолс  */
+    onCharLoaded = (char) => {
+        this.setState({
+            char, 
+            loading: false
+        })
+    }
+
+    //Метод для устновки ошибки
+    onError = () => {
+        this.setState({
+            loading: false, // ставив в false потому что если произошла ошибка то нет загрузки
+            error: true
+        })
     }
 
     updateChar = () => {
         const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
         this.marvelService
-            /*.getAllCharacters()
-            .then(res => console.log(res))  Выводим в массив результат по получению всех персонажей. Мы получили ДВА МАССИВА, И
-В ПЕРВОМ И ВО ВТОРОМ ХРАНИЛИСЬ ОБЪЕКТЫ С ДАНЫМИ О ГЕРОЯХ, НО ОЧЕНЬ ВАЖНО ЧТО ДВА МАССИВА МЫ ПОЛУЧИЛИ ПОТОМУ ЧТО МЫ СДЕЛАЛИ НЕСКОЛЬКО
-ЗАПРОСОВ И ПОЛУЧИЛИ НЕСКОЛЬКО ОТВЕТОВ ИЗ-ЗА ТОГО ЧТОВЫЗЫВАЕМ this.updateChar В НАШЕМ КОНСТРУКТОРЕ. ЭТО УДАРИТ ПО НАШЕЙ ОПТИМИЗАЦИИ
-И ЭТО ПРИЧИНА ПОЧЕМУ ЛУЧШЕ НЕ РАБОТАТЬ С КОНСТРУКТОРАМИ И МЫ ЭТО ПОТОМ ИСПРАВИМ */
             .getCharacter(id) 
-            .then(this.onChatLoaded) /* Когда мы используем промисы и цепочка идет через then то у нас в эту фцию приходит аргумент
-и если у нас в then стоит ссылка на фукцию как this.onChatLoaded, то аргумент который прийдет в then автоматически будет подставляться
-туда где сейчас находится this.onChatLoaded, те в метод onChatLoaded. Он прийдет сюда как в (char) и запишется уже во внутрь стейта*/ 
+            .then(this.onCharLoaded)
+            .catch(this.onError)//наша ошибка в onError можем произойти внутри запроса, по этому делаем catch  
     } 
 
-    /* И последнее - это вытгивание данных из стейта. Так как все эти сущности лежат у нас в объекте char - {name, description, thumbnail, homepage, wiki}
-то мы просто подставм char. Мы вытаскиваем сущности из свойства char*/
     render() {
-        const {char: {name, description, thumbnail, homepage, wiki}} = this.state;
+        /* Из-за того что мы создали viem мы можем здесь не вытаскивать все эти данные(name, descr ...) */
+        const {char, loading, error} = this.state;
 
-/* Запишу здесь ИТОГ урока. Если наше приложение взаимодействует с сервером, то нужно ОТДЕЛЯТЬ СЕТЕВЫЕ КОМОПНЕНТЫ ОТ ОСНОВНОГО КОДА,
-ТАК МЫ СМОЖЕМ ИХ ИСПОЛЬЗОВАТЬ В СОВЕРШЕННО РАЗНЫХ ЧАСТЯХ ПРИЛОЖЕНИЯ И СДЕЛАТЬ КОД НАМНОГО ЧИЩЕ. У нас сетевая часть находится в
-МАРВЕЛ СЕРВИС И ИМЕННО ОНА БУДЕТ ОТВЕЧАТЬ ЗА ТРАНСФОРМАЦИЮ ДАННЫХ*/
+        /* Теперь будем заниматься вычислением нашего состояния, обычно когда нужно писать много условий по состоянию
+компонента то его пишут выше return ! Потому что если будем писать внутри return - будет каша */
+        const errorMessage = error ? <ErrorMessage/> : null // Теперь здесь содержиться либо ошибка либо ничего
+        const spinner = loading ? <Spinner/> : null // Тоже самое делаем со спинером
+        const content = !(loading || error) ? <View char={char}/> : null /* Контент нужно помещать сразу после загрузки, 
+но если нет ошибки, !(loading || error) - и звучит это так - если нет загрузки или нет ошибки */
+/* ПОДВОЖУ ИТОГИ УРОКА: Когда картинка загружается - подставляется спиннер с загрузки, 
+                        Когда контент загрузился - подставляется герой - все нормально,
+                        Когда я сменил одну букву в ключе к моему АПИ - появляется робот с ошибкой */
 
 
+        /*Логика такая - если у нас идет состояние загрузки то мы импортировали компонент Спинера и помещаем в него
+наш скаченный с интернета спинер 
+        if (loading) {
+            return <Spinner/>
+        }  Этот код больше не нужен*/
+/* Если у нас loagind в true то мы подставляем Spinner, а если нет, то подставляем наш View(это обычная верстка которую
+мы просто вырезали в случае логание инета у пользователя что бы он понимал что что-то происходит) и этому View мы
+добавляем ПРОПЕРТИ char {loading ? <Spinner/> : <View char={char}/>} 
+ВСЕ ЧТО Я НАПИСАЛ ВЫШЕ УЖЕ НЕАКТУАЛЬНО ПОТОМУ ЧТО МЫ ВСЕ НАШИ СРАВНЕНИЕ И УСЛОВИЕ НАПИСАЛИ В ПЕРЕМЕННЫЕ*/
         return (
             <div className="randomchar">
-                <div className="randomchar__block">
-                    <img src={thumbnail} alt="Random character" className="randomchar__img"/>
-                    <div className="randomchar__info">
-                        <p className="randomchar__name">{name}</p>
-                        <p className="randomchar__descr">
-                            {description}
-                        </p>
-                        <div className="randomchar__btns">
-                            <a href={homepage} className="button button__main">
-                                <div className="inner">homepage</div>
-                            </a>
-                            <a href={wiki} className="button button__secondary">
-                                <div className="inner">Wiki</div>
-                            </a>
-                        </div>
-                    </div>
-                </div>
+                {errorMessage}
+                {spinner}
+                {content}
                 <div className="randomchar__static">
                     <p className="randomchar__title">
                         Random character for today!<br/>
@@ -82,6 +91,34 @@ class RandomChar extends Component {
             </div>
         )
     }
+}
+
+/*Этот компонент будет отображать кусочек нашей верстки.
+  Это будет простой рендереющий компонент, который получает данные как объект, в нем нет никакой логике, он просто как
+объект получает аргумент с данными, принимает его в себя и возвращает участок какой-то верстки для того что бы просто
+отоброзить. Все главные запросы, приобразование у нас идут в основном компоненте RandomChar */
+const View = ({char}) => {
+    const {name, description, thumbnail, homepage, wiki} = char
+
+    return (
+        <div className="randomchar__block">
+            <img src={thumbnail} alt="Random character" className="randomchar__img"/>
+            <div className="randomchar__info">
+                <p className="randomchar__name">{name}</p>
+                <p className="randomchar__descr">
+                    {description}
+                </p>
+                <div className="randomchar__btns">
+                    <a href={homepage} className="button button__main">
+                        <div className="inner">homepage</div>
+                    </a>
+                    <a href={wiki} className="button button__secondary">
+                        <div className="inner">Wiki</div>
+                    </a>
+                </div>
+            </div>
+    </div>
+    )
 }
 
 export default RandomChar;
--- src/services/MarvelService.jsx
+++ src/services/MarvelService.jsx
@@ -16,20 +16,16 @@ class MarvelService {
         const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
         return res.data.results.map(this._transformCharacter)
     }
-    /* res в этом случае это БОЛЬШОЙ ОБЪЕКТ ГДЕ ХРАНИТСЯ МАССИВ С РЕЗУЛЬТАТАМИ
-       Так как это массив, то мы можем применить метод map() - ЧТО БЫ СФОРМИРОВАТЬ МАССИВ С НОВЫМИ ОБЪЕКТАМИ */
 
     getCharacter = async (id) => {
         const res =  await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
         return this._transformCharacter(res.data.results[0])
     }
-    /* А сюда помещаем повторяющейся участок кода */
     
-    /* Теперь я говорю что здесь будет приходить объект персонажа */
     _transformCharacter = (char) => {
         return {
             name: char.name,
-            description: char.description,
+            description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character', // есди больше 210 символов в катинке что бы не вылазила
             thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
             homepage: char.urls[0].url,
             wiki: char.urls[1].url
