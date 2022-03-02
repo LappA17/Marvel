0b52d626402cddb386d8583e221ab119c3376893
Ruslan <ruslan.postoyuk@gmail.com>
2022-03-02 23:06:56 +0100

    115. ДЗ: 1) CharList 2)Если не фото у героя то поцентру 3)Try it - переключает героя

--- src/components/randomChar/RandomChar.jsx
+++ src/components/randomChar/RandomChar.jsx
@@ -1,33 +1,39 @@
 import { Component } from 'react';
-import MarvelService from '../../services/MarvelService'; 
-import './randomChar.scss';
-import mjolnir from '../../resources/img/mjolnir.png';
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
+import MarvelService from '../../services/MarvelService';
+
+import './randomChar.scss';
+import mjolnir from '../../resources/img/mjolnir.png';
 
 class RandomChar extends Component {
-
     state = {
         char: {},
         loading: true,
         error: false
     }
-    
+
     marvelService = new MarvelService();
 
     componentDidMount() {
-        this.updateChar()
-        this.timerId = setInterval(this.updateChar, 3000)
+        this.updateChar();
+        // this.timerId = setInterval(this.updateChar, 15000);
     }
 
     componentWillUnmount() {
-        clearInterval(this.timerId)
+        clearInterval(this.timerId);
     }
 
     onCharLoaded = (char) => {
         this.setState({
             char, 
             loading: false
+        })
+    }
+
+    onCharLoading = () => {
+        this.setState({
+            loading: true
         })
     }
 
@@ -40,18 +46,19 @@ class RandomChar extends Component {
 
     updateChar = () => {
         const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
+        this.onCharLoading();
         this.marvelService
-            .getCharacter(id) 
+            .getAllCharacters(id)
             .then(this.onCharLoaded)
-            .catch(this.onError)  
-    } 
+            .catch(this.onError);
+    }
 
     render() {
-        const {char, loading, error} = this.state;
 
-        const errorMessage = error ? <ErrorMessage/> : null
-        const spinner = loading ? <Spinner/> : null
-        const content = !(loading || error) ? <View char={char}/> : null 
+        const {char, loading, error} = this.state;
+        const errorMessage = error ? <ErrorMessage/> : null;
+        const spinner = loading ? <Spinner/> : null;
+        const content = !(loading || error) ? <View char={char}/> : null;
 
         return (
             <div className="randomchar">
@@ -66,7 +73,7 @@ class RandomChar extends Component {
                     <p className="randomchar__title">
                         Or choose another one
                     </p>
-                    <button className="button button__main">
+                    <button onClick={this.updateChar} className="button button__main">
                         <div className="inner">try it</div>
                     </button>
                     <img src={mjolnir} alt="mjolnir" className="randomchar__decoration"/>
@@ -77,11 +84,18 @@ class RandomChar extends Component {
 }
 
 const View = ({char}) => {
-    const {name, description, thumbnail, homepage, wiki} = char
+    const {name, description, thumbnail, homepage, wiki} = char;
+
+    /* Код ниже нужен для того что бы когда появлялся персонаж без картинки - у него надпись image not found не вылезала
+за длину экрана,а была видна чуть левее центра. Те с кавер меняется на contain */
+    let imgStyle = {'objectFit' : 'cover'};
+    if (thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
+        imgStyle = {'objectFit' : 'contain'};
+    }
 
     return (
         <div className="randomchar__block">
-            <img src={thumbnail} alt="Random character" className="randomchar__img"/>
+            <img src={thumbnail} alt="Random character" className="randomchar__img" style={imgStyle}/>
             <div className="randomchar__info">
                 <p className="randomchar__name">{name}</p>
                 <p className="randomchar__descr">
@@ -96,7 +110,7 @@ const View = ({char}) => {
                     </a>
                 </div>
             </div>
-    </div>
+        </div>
     )
 }
 
