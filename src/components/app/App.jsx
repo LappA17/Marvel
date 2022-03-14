be170dd54a9db02ea030c7ede85386423c877173
Ruslan <ruslan.postoyuk@gmail.com>
2022-03-14 23:55:11 +0100

    132. Практика. Перепишем весь проект на хуки

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import { useState } from "react";
 import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
@@ -7,17 +7,20 @@ import ErrorBoundary from "../errorBoundary/ErrorBoundary";
 
 import decoration from '../../resources/img/vision.png';
 
-class App extends Component {
-    state = {
+const App = () => {
+    const [selectedChar, setChar] = useState(null)
+    /* state = {
         selectedChar: null
+    } */
+    const onCharSelected = (id) => {
+        setChar(id)
     }
-
-    onCharSelected = (id) => {
+    /* onCharSelected = (id) => {
         this.setState({
             selectedChar: id
         })
-    }
-   render () {
+    } */
+  // render () {
     return (
         <div className="app">
             <AppHeader/>
@@ -27,17 +30,17 @@ class App extends Component {
                 </ErrorBoundary>
                 <div className="char__content">
                     <ErrorBoundary>
-                        <CharList onCharSelected={this.onCharSelected}/>
+                        <CharList onCharSelected={onCharSelected}/>
                     </ErrorBoundary>
                     <ErrorBoundary>
-                        <CharInfo charId={this.state.selectedChar}/>
+                        <CharInfo charId={selectedChar}/>
                     </ErrorBoundary>
                 </div>
                 <img className="bg-decoration" src={decoration} alt="vision"/>
             </main>
         </div>
     )
-   }
+  // }
 }
 
 export default App;
--- src/components/charList/CharList.jsx
+++ src/components/charList/CharList.jsx
@@ -1,4 +1,4 @@
-import {Component} from 'react';
+import {useState, useEffect, useRef} from 'react';
 import PropTypes from 'prop-types';
 
 import Spinner from '../spinner/Spinner';
@@ -6,65 +6,106 @@ import ErrorMessage from '../errorMessage/ErrorMessage';
 import MarvelService from '../../services/MarvelService';
 import './charList.scss';
 
-class CharList extends Component {
-
-    state = {
+/* Так как у нас есть Компонент который принимает пропс props.onCharSelected(item.id), то мы передаем как аргумент props - это объект с 
+нашими свойствами*/
+const CharList = (props) => {
+    const [charList, setCharList] = useState([])
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(false)
+    const [newItemLoading, setNewItemLoading] = useState(false)
+    const [offset, setOffset] = useState(210)
+    const [charEnded, setCharEnded] = useState(false)
+    /* state = {
         charList: [],
         loading: true,
         error: false,
         newItemLoading: false,
         offset: 210,
         charEnded: false
-    }
+    } */
     
-    marvelService = new MarvelService();
+    const marvelService = new MarvelService(); // marvelService - объект, который констурируется при помощи класса MarvelService
 
-    componentDidMount() {
+    useEffect(() => {
+        onRequest() /* onRequest - у нас стрелочная фция, но мы запускаем ее выше. ДЕЛО В ТОМ ЧТО useEffect ЗАПУСКАЕТСЯ ПОСЛЕ РЕНДЕРА,
+ТО-ЕСТЬ ПОСЛЕ ТОГО КАК НАША ФУНКЦИЯ СУЩЕСТУЕТ УЖЕ ВНУТРИ НАШЕГО КОМПОНЕНТА */
+    }, []) /* Когда пустой массив то функция выполнится только ОДИН РАЗ
+              Так же у нас эта функция меняться не будет, мы туда в массив onRequest не передаем
+              И мы специально вручную оставили пустой массив что бы с эмитировать метод componentDidMount(), а если мы добавим фцию в 
+            зависимость то мы можем запустить бесконечный цикл(потому что мы запустим наш Компонент, он создаться, потом запустит
+            фцию onRequest, у нас будет выполнятся запрос и его успешный вариант в then(onCharListLoaded) он меняет state, когда
+            у нас меняется state то у нас запускается ПЕРЕРЕНДЕРИНГ КОМПОНЕНТА и пойдет все по новой и бесконечный цикл запросов) */
+    /* componentDidMount() {
         this.onRequest();
-    }
+    } */
 
-    onRequest = (offset) => {
+    const onRequest = (offset) => {
+        onCharListLoading();
+        marvelService.getAllCharacters(offset)
+            .then(onCharListLoaded)
+            .catch(onError)
+    } 
+    /* onRequest = (offset) => {
         this.onCharListLoading();
         this.marvelService.getAllCharacters(offset)
             .then(this.onCharListLoaded)
             .catch(this.onError)
-    }
+    } */
 
-    onCharListLoading = () => {
+    const onCharListLoading = () => {
+        setNewItemLoading(true)
+    }
+    /* onCharListLoading = () => {
         this.setState({
             newItemLoading: true
         })
-    }
+    } */
 
-    onCharListLoaded = (newCharList) => {
+    const onCharListLoaded = (newCharList) => {
         let ended = false;
         if (newCharList.length < 9) {
             ended = true;
         }
 
-        this.setState(({offset, charList}) => ({
+        /* this.setState(({offset, charList}) => ({
             charList: [...charList, ...newCharList],
             loading: false,
             newItemLoading: false,
             offset: offset + 9,
             charEnded: ended
-        }))
+        })) */
+        setCharList(charList => [...charList, ...newCharList]) //С коллбеком что бы была последованность изменение наших стейтов
+        setLoading(loading => false) //здесь можно так же как и в setError просто оставить false без коллбека
+        setNewItemLoading(newItemLoading => false)
+        setOffset(offset => offset + 9)
+        setCharEnded(charEnded => ended)
     }
 
-    onError = () => {
-        this.setState({
+    const onError = () => {
+        /* this.setState({
             error: true,
             loading: false
-        })
+        }) */
+        setError(true) // здесь не важно что было в прошлом стейте, потому что если ошибка то точно нужно true
+        setLoading(loading => false)
     }
 
-    itemRefs = [];
-
-    setRef = (ref) => {
+    /* useRef можно использовать только НА ВЕРХНЕМ УРОВНЕ КОМПОНЕНТА(НИ В КАКИХ УСЛОВИЯХ, ВНУТРИ ФУНКЦИЯХ ИЛИ ЦИКЛАХ) */
+    //itemRefs = [];
+    const itemRefs = useRef([])
+/* Если всмотреть в код, то можно понять что Ваня не использовал никак свойство current у Рефа. Ваня пушил прям на прямую в itemRefs
+а в нем нет свойства current
+   Но когда мы создаем при помощи useRef, то current обязательно будет(потому что будет объект с таким свойством) */
+    /* setRef = (ref) => {
         this.itemRefs.push(ref);
-    }
+    } Так мы уже не сможем поместить ref в нашу верстку. Мы пишем фцию прям внутри ref={} */
 
-    focusOnItem = (id) => {
+    /* itemRefs.current - это значит что у нас в itemRefs лежит вот таком массив, внутри свойство current. ТОЕСТЬ НАШ МАССИВ БУДЕТ СКЛАДЫВАТЬСЯ
+ВО ВНУТРЬ itemRefs.current(свойство объекта). Который я буду перебиратьи дальше у каждого элемента внутри этого массива(это будет 
+ссылка на ДОМ элемент) я буду убирать класс 
+    Дальше внутри этого массива(и мы перемещае id с itemRefs[id].current на itemRefs.current[id]), внутри этого массива ссылок мы находим
+элемент по id и добавляем класс и фокус*/
+    const focusOnItem = (id) => {
         // Я реализовал вариант чуть сложнее, и с классом и с фокусом
         // Но в теории можно оставить только фокус, и его в стилях использовать вместо класса
         // На самом деле, решение с css-классом можно сделать, вынеся персонажа
@@ -72,14 +113,14 @@ class CharList extends Component {
         // и не факт, что мы выиграем по оптимизации за счет бОльшего кол-ва элементов
 
         // По возможности, не злоупотребляйте рефами, только в крайних случаях
-        this.itemRefs.forEach(item => item.classList.remove('char__item_selected'));
-        this.itemRefs[id].classList.add('char__item_selected');
-        this.itemRefs[id].focus();
+        itemRefs.current.forEach(item => item.classList.remove('char__item_selected'));
+        itemRefs.current[id].classList.add('char__item_selected');
+        itemRefs.current[id].focus();
     }
 
     // Этот метод создан для оптимизации, 
     // чтобы не помещать такую конструкцию в метод render
-    renderItems(arr) {
+    function renderItems(arr) {
         const items =  arr.map((item, i) => {
             let imgStyle = {'objectFit' : 'cover'};
             if (item.thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
@@ -90,16 +131,21 @@ class CharList extends Component {
                 <li 
                     className="char__item"
                     tabIndex={0}
-                    ref={this.setRef}
+                    ref={el => itemRefs.current[i] = el}/* Здесь будет callback ref который в себя будет принимать элемент на котором он был вызван, а именно li
+А так как у нас чуть выше перебор массива с помощью map, то li у нас создает ВНУТРИ ЦИКЛОВ. По-этому мы задаем ссылку на элемент el - те
+это четкий элемент на котором происходит действие и так как у нас itemsRefs.current - это массив, то мы можем здесь использовать не
+push который может дать ошибку, а itemRefs.current[i] = el то-есть я просто элементы по порядку в этом массиве складываю в itemRefs.current
+и в этом коде itemRefs.current[i] = el el-идет как ссылка на Дом элемент и у нас в этом массиве itemRefs.current будет формироваться массив
+ссылок на элементы которые будут последовательно формироваться  */
                     key={item.id}
                     onClick={() => {
-                        this.props.onCharSelected(item.id);
-                        this.focusOnItem(i);
+                        props.onCharSelected(item.id); // У нас есть Компонент который принимает props
+                        focusOnItem(i);
                     }}
                     onKeyPress={(e) => {
                         if (e.key === ' ' || e.key === "Enter") {
-                            this.props.onCharSelected(item.id);
-                            this.focusOnItem(i);
+                            props.onCharSelected(item.id); 
+                            focusOnItem(i);
                         }
                     }}>
                         <img src={item.thumbnail} alt={item.name} style={imgStyle}/>
@@ -115,11 +161,12 @@ class CharList extends Component {
         )
     }
 
-    render() {
+    //render() {
 
-        const {charList, loading, error, offset, newItemLoading, charEnded} = this.state;
+        /*У нас эти переменные и так существуют внутри функции по этом удаляем
+        const {charList, loading, error, offset, newItemLoading, charEnded} = this.state; */
         
-        const items = this.renderItems(charList);
+        const items = renderItems(charList);
 
         const errorMessage = error ? <ErrorMessage/> : null;
         const spinner = loading ? <Spinner/> : null;
@@ -134,12 +181,12 @@ class CharList extends Component {
                     className="button button__main button__long"
                     disabled={newItemLoading}
                     style={{'display': charEnded ? 'none' : 'block'}}
-                    onClick={() => this.onRequest(offset)}>
+                    onClick={() => onRequest(offset)}>
                     <div className="inner">load more</div>
                 </button>
             </div>
         )
-    }
+   // }
 }
 
 CharList.propTypes = {
